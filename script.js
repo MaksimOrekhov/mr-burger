db6417cfd8a09629de933a3d8df52b5e5ff2b553
Maksim Orekhov <timberwolf.max@gmail.com>
2017-11-11 00:37:30 +0300

    добавлена верстка слайдера

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 let hover = document.querySelector('.img__ingredients')
 let list = document.querySelector('.ingredients__list')
+let listClose = document.querySelector('.close__cross')
 
 hover.addEventListener('pointerenter', function(e) {
   list.style.opacity = 1;
 });
 
 hover.addEventListener('pointerleave', function(e) {
+  list.style.opacity = 0;
+});
+
+hover.addEventListener('touchstart', function() {
+  list.style.opacity = 1;
+});
+
+hover.addEventListener('touchend', function() {
+  list.style.opacity = 1;
+});
+
+listClose.addEventListener('touchstart', function() {
   list.style.opacity = 0;
 });
