9900994816719dcdc33d7be2e5c0c50641706fd0
Maksim Orekhov <timberwolf.max@gmail.com>
2017-11-11 16:28:57 +0300

    реализована функция гамбургер-меню

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 let hover = document.querySelector('.img__ingredients')
 let list = document.querySelector('.ingredients__list')
 let listClose = document.querySelector('.close__cross')
+let burgerButton = document.querySelector('.burger__menu')
+let tabletsMenu = document.querySelector('.menu--tablets')
+let tabletsMenuClose = document.querySelector('.menu__close')
 
-hover.addEventListener('pointerenter', function(e) {
+//работа гамбургер-меню на планшетах/телефонах
+burgerButton.addEventListener('touchstart', function() {
+  tabletsMenu.style.display = "block";
+});
+
+tabletsMenuClose.addEventListener('touchstart', function() {
+  tabletsMenu.style.display = "none";
+});
+
+// работа выпадающего меню с ингредиентами
+hover.addEventListener('pointerenter', function() {
   list.style.opacity = 1;
 });
 
-hover.addEventListener('pointerleave', function(e) {
+hover.addEventListener('pointerleave', function() {
   list.style.opacity = 0;
 });
 
